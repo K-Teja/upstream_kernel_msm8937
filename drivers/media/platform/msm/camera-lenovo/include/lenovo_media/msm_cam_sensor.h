f8c11599de76fe2bb249ffea8233ff9e150f4804
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-11-14 21:04:31 +0800

    include: export lenovo camera headers
    * lenovo specific camera headers needed for building camera hal
    
    Change-Id: Ic6a67d412816d378e8d92fc61ec5ec1c8aa17d54
    Signed-off-by: teja98 <tejachaitu96@gmail.com>

--- drivers/media/platform/msm/camera-lenovo/include/lenovo_media/msm_cam_sensor.h
+++ drivers/media/platform/msm/camera-lenovo/include/lenovo_media/msm_cam_sensor.h
@@ -2,7 +2,7 @@
 #define __LINUX_MSM_CAM_SENSOR_H
 
 #include <linux/v4l2-mediabus.h>
-#include "./msm_camsensor_sdk.h"
+#include <media/msm_camsensor_sdk_lenovo.h>
 
 #include <linux/types.h>
 #include <linux/i2c.h>
